3d1a71ff2fc8bc432ee279354e40c20ba688af5c
Jose Aguilar <jaguilarny@gmail.com>
2022-09-04 23:59:16 -0400

    commit tree c62e30a15b73f617c43d28d13afa15ee4b6881c7

---
+++ cash_register_spec.rb
@@ -0,0 +1,16 @@
+example_id                            | status | run_time        |
+------------------------------------- | ------ | --------------- |
+./spec/cash_register_spec.rb[1:1:1]   | passed | 0.00028 seconds |
+./spec/cash_register_spec.rb[1:1:2]   | passed | 0.00011 seconds |
+./spec/cash_register_spec.rb[1:2:1]   | passed | 0.00005 seconds |
+./spec/cash_register_spec.rb[1:3:1]   | passed | 0.00197 seconds |
+./spec/cash_register_spec.rb[1:3:2]   | passed | 0.00059 seconds |
+./spec/cash_register_spec.rb[1:3:3]   | passed | 0.00008 seconds |
+./spec/cash_register_spec.rb[1:4:1:1] | passed | 0.00007 seconds |
+./spec/cash_register_spec.rb[1:4:1:2] | passed | 0.00005 seconds |
+./spec/cash_register_spec.rb[1:4:1:3] | passed | 0.00061 seconds |
+./spec/cash_register_spec.rb[1:4:2:1] | passed | 0.00006 seconds |
+./spec/cash_register_spec.rb[1:5:1:1] | passed | 0.00005 seconds |
+./spec/cash_register_spec.rb[1:5:2:1] | passed | 0.00007 seconds |
+./spec/cash_register_spec.rb[1:6:1]   | passed | 0.00005 seconds |
+./spec/cash_register_spec.rb[1:6:2]   | passed | 0.00008 seconds |
--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+class CashRegister
+    attr_writer :items
+    attr_accessor :discount, :total, :last_transaction
 
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.items.concat([title] * quantity)
+        self.last_transaction = price * quantity
+        self.total += price * quantity
+    end
+
+    def apply_discount
+        if self.discount != 0
+            calculated_discount = (self.discount / 100.to_f) * self.total
+            self.total = (self.total - calculated_discount).to_i
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+    
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        self.total -= self.last_transaction
+    end
+end
